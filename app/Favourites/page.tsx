985d38a4dc46299493e9b11fda26fc5dd82ff9d3
Eman Ali Abbasi <113726637+maani31@users.noreply.github.com>
2024-03-24 23:25:46 +0500

    new feature

--- app/Favourites/page.tsx
+++ app/Favourites/page.tsx
@@ -5,46 +5,45 @@ import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
-export default function page() {
+export default function Page() {
   const favData = useAppSelector((state) => state.fav);
   const [domLoaded, setDomLoaded] = useState(false);
-  const dispatch = useAppDispatch()
-  const handleRemovefav = (id:number, title:string , strMealThumb:string) => {
-    // Dispatch action to remove from todo
-    dispatch(removefav({idMeal: id, strMeal: title, strMealThumb: strMealThumb}));
+  const dispatch = useAppDispatch();
+
+  const handleRemovefav = (id: number, title: string, strMealThumb: string) => {
+    // Dispatch action to remove from fav
+    dispatch(removefav({ idMeal: id, strMeal: title, strMealThumb: strMealThumb }));
   };
+
   // UI Doesn't match errror solve
   useEffect(() => {
     setDomLoaded(true);
   }, []);
+
   return (
     <>
       <h1 className="text-3xl font-bold flex justify-center py-4">Favourites</h1>
       <div className="flex justify-center align-middle">
-      {favData.length !== 0 && domLoaded ? (
-        <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 justify-center align-middle px-4">
-          {favData.map((item) => {
-            return (
-                <div key={item.idMeal} className="flex flex-col justify-center align-middle sm:h-96">
-                  <Image
-                    src={item.strMealThumb}
-                    alt=""
-                    width={300}
-                    height={300}
-                  />
-                  <h1 className="sm:text-sm text-xs">{item.strMeal}</h1>
-                  <Button
-                    onClick={()=>handleRemovefav(item.idMeal, item.strMeal, item.strMealThumb)}
-                  >
-                    Remove
-                  </Button>
-                </div>
-            );
-          })}
-        </div>
-      ) : (
-        <p>Nothing to Do</p>
-      )}
+        {favData.length !== 0 && domLoaded ? (
+          <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 justify-center align-middle px-4">
+            {favData.map((item) => (
+              <div key={item.idMeal} className="flex flex-col justify-center align-middle sm:h-96">
+                <Image
+                  src={item.strMealThumb}
+                  alt=""
+                  width={300}
+                  height={300}
+                />
+                <h1 className="sm:text-sm text-xs">{item.strMeal}</h1>
+                <Button onClick={() => handleRemovefav(item.idMeal, item.strMeal, item.strMealThumb)}>
+                  Remove
+                </Button>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>Nothing to Do</p>
+        )}
       </div>
     </>
   );
