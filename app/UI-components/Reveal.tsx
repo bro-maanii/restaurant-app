50fe5a4b7f65c8c1bacfacd887ab648f83326dfb
Eman Ali Abbasi <113726637+maani31@users.noreply.github.com>
2024-05-19 00:20:17 +0500

    Add Basic Animation

---
+++ app/UI-components/Reveal.tsx
@@ -0,0 +1,35 @@
+"use client"
+import { motion, useAnimation, useInView } from 'framer-motion';
+import React, { useEffect, useRef } from 'react'
+
+export default function 
+({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    const ref =useRef(null)
+    const IsInView = useInView(ref , {once: false});
+    const maincontrol=useAnimation();
+    useEffect(()=>{
+        if(IsInView){
+            maincontrol.start("visible")
+        }
+    },[IsInView])
+  return (
+    <div ref={ref}>
+        <motion.div 
+        variants={{
+            hidden: { opacity: 0 , y: 75},
+            visible: { opacity: 1 , y: 0},
+        }}
+        initial="hidden"
+        animate={maincontrol}
+        viewport={{ once: true}}
+        transition={{ duration: 0.5 ,delay: 0.75}}
+        >
+            {children}
+        </motion.div>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,46 @@
+"use client";
 import AboutUS from "./UI-components/AboutUS";
 import BookSeat from "./UI-components/BookSeat";
 import Hero from "./UI-components/Hero";
 import Hero2 from "./UI-components/Hero2";
+import Reveal from "./UI-components/Reveal";
 import Team from "./UI-components/Team";
 import TestimonialPage from "./UI-components/Testinominals/Testinomial";
 
 export default function Home() {
   return (
-    <>
-      <div className="wapper">
+    <><div className="wapper">
+      <Reveal>
+      
         <Hero />
+      
+      </Reveal>
       </div>
       <div className="bg-red-800 py-5">
+        <Reveal>
         <Hero2 />
+        </Reveal>
       </div>
       <div className="wapper">
+        <Reveal>
         <AboutUS />
+        </Reveal>
       </div>
       <div className="bg-red-200 my-8">
+        <Reveal>
         <Team />
+        </Reveal>
+
       </div>
       <div className="wapper">
+        <Reveal>
         <TestimonialPage />  
+        </Reveal>
       </div>
       <div className="contact-bg my-8">
+        <Reveal>
         <BookSeat />
+        </Reveal>
       </div>
       
     </>
