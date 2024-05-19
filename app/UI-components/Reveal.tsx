d9d5b53377f689cf00fb41cde520c73658b71af8
bro-maanii <emanaliabbasi31@gmail.com>
2024-05-19 17:24:44 +0500

    extra code removingg

--- app/UI-components/Reveal.tsx
+++ app/UI-components/Reveal.tsx
@@ -1,35 +1,34 @@
-"use client"
-import { motion, useAnimation, useInView } from 'framer-motion';
-import React, { useEffect, useRef } from 'react'
+"use client";
+import { motion, useAnimation, useInView } from "framer-motion";
+import React, { useEffect, useRef } from "react";
 
-export default function Reveal
-({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    const ref =useRef(null)
-    const IsInView = useInView(ref , {once: false});
-    const maincontrol=useAnimation();
-    useEffect(()=>{
-        if(IsInView){
-            maincontrol.start("visible")
-        }
-    },[IsInView])
+export default function Reveal({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const ref = useRef(null);
+  const IsInView = useInView(ref, { once: false });
+  const maincontrol = useAnimation();
+  useEffect(() => {
+    if (IsInView) {
+      maincontrol.start("visible");
+    }
+  }, [IsInView]);
   return (
     <div ref={ref}>
-        <motion.div 
+      <motion.div
         variants={{
-            hidden: { opacity: 0 , y: 75},
-            visible: { opacity: 1 , y: 0},
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
         }}
         initial="hidden"
         animate={maincontrol}
-        viewport={{ once: true}}
-        transition={{ duration: 0.5 ,delay: 0.75}}
-        >
-            {children}
-        </motion.div>
+        viewport={{ once: true }}
+        transition={{ duration: 0.5, delay: 0.75 }}
+      >
+        {children}
+      </motion.div>
     </div>
-  )
+  );
 }
